45bf7fe26a6ecd9bd2d9cba3c1bbfbf385c64af6
ryan <ryanwels94@gmail.com>
2016-03-29 11:59:40 +0200

    feat: Lyrics now have actino and toggle

--- UB02/app/components/scooter-lyrics.js
+++ UB02/app/components/scooter-lyrics.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: ['scooterlyric']
+  classNames: ['scooterlyric'],
+  showLyrics: false,
+  actions: {
+    lyricShow() {
+      this.set('showLyrics', true);
+    },
+    lyricHide() {
+      this.set('showLyrics', false);
+    }
+  }
 });
